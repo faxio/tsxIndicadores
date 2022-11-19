7720b2db445dac2d0ec7226ad71656e97a956c99
MillarCD <diego12millar@gmail.com>
2022-11-18 22:19:02 -0300

    base de datos casi fucionada con la de evidencia

--- Database/init_db.sql
+++ Database/init_db.sql
@@ -1,3 +1,5 @@
+DROP DATABASE IF EXISTS innoving;
+
 CREATE DATABASE innoving;
 
 USE innoving;
@@ -12,4 +14,50 @@ CREATE TABLE Indicadores (
   FOREIGN KEY (nombre_eje) REFERENCES Ejes(nombre)
 );
 
+CREATE TABLE Estados (estado VARCHAR(100) NOT NULL PRIMARY KEY);
 
+CREATE TABLE publicacion (
+  publicacion_id int not null AUTO_INCREMENT PRIMARY KEY,
+  issn_doi varchar(100) not null,
+  titulo varchar(100),
+  autores varchar(100),
+  revista varchar(100),
+  autores_extranjeros TINYINT(1),
+  indexacion varchar(100), 
+  anio varchar(100),
+  citaciones varchar(300),
+  clasificacion varchar(100),
+  disciplina varchar(100),
+  estado VARCHAR(100) NOT NULL,
+  FOREIGN KEY (estado) REFERENCES Estados(estado)
+);
+
+CREATE TABLE Variables (
+  id VARCHAR(10) NOT NULL PRIMARY KEY,
+  descripcion VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE Indicadores_Variables (
+  id_ind_var VARCHAR(10) NOT NULL PRIMARY KEY,
+  id_indicador VARCHAR(10) NOT NULL,
+  id_variable VARCHAR(10) NOT NULL,
+  FOREIGN KEY (id_indicador) REFERENCES Indicadores(id),
+  FOREIGN KEY (id_variable) REFERENCES Variables(id)
+);
+
+CREATE TABLE Publicaciones_Variables (
+  id_pub_var VARCHAR(10) NOT NULL PRIMARY KEY,
+  valor INT NOT NULL,
+  id_publicacion INT NOT NULL,
+  id_variable VARCHAR(10) NOT NULL,
+  FOREIGN KEY (id_publicacion) REFERENCES publicacion(publicacion_id),
+  FOREIGN KEY (id_variable) REFERENCES Variables(id)
+);
+
+CREATE TABLE Publicaciones_Indicadores (
+  id_pub_ind VARCHAR(10) NOT NULL PRIMARY KEY,
+  id_publicacion INT NOT NULL,
+  id_indicador VARCHAR(10) NOT NULL,
+  FOREIGN KEY (id_publicacion) REFERENCES publicacion(publicacion_id),
+  FOREIGN KEY (id_indicador) REFERENCES Indicadores(id)
+);
--- Database/insert_data.sql
+++ Database/insert_data.sql
@@ -26,3 +26,92 @@ INSERT INTO Indicadores VALUES
   "Como su nombre lo dice, este indicador es el responsable de señalar el porcentaje de coautores extranjeros",
   "El ultimo eje"
 );
+
+
+INSERT INTO Estados VALUES ("Verificado"), ("Rechazado"), ("En revicion");
+
+/**  adaptar con la de eviencia */
+INSERT INTO publicacion VALUES
+(
+  1,
+  "doi 1",
+  "titulo 1",
+  "autores 1",
+  "revista 1",
+  1,
+  "indexacion 1",
+  "anio 1",
+  "citaicon",
+  "clasificacion 1",
+  "descipcion 3",
+  "Rechazado"
+),
+(
+  2,
+  "doi 2",
+  "titulo 2",
+  "autores 2",
+  "revista 2",
+  1,
+  "indexacion 2",
+  "anio 2",
+  "citaicon 2",
+  "clasificacion 2",
+  "descipcion 2",
+  "Verificado"
+),
+(
+  3,
+  "doi 3",
+  "titulo 3",
+  "autores 3",
+  "revista 3",
+  1,
+  "indexacion 3",
+  "anio 3",
+  "citaicon 3",
+  "clasificacion 3",
+  "descipcion 3",
+  "Verificado"
+);
+
+INSERT INTO Variables VALUES
+(
+  "V1",
+  "publicacion"
+),
+(
+  "V2",
+  "publicacion de ingenieria"
+),
+(
+  "V3",
+  "Coautor exxtrangero"
+);
+
+
+INSERT INTO Publicaciones_Indicadores VALUES
+(
+  "PM1",
+  2,
+  "M25"
+),
+(
+  "PM2",
+  3,
+  "M25"
+);
+
+INSERT INTO Publicaciones_Variables VALUES 
+(
+  "PV1",
+  1,
+  2,
+  "V1"
+),
+(
+  "PV2",
+  1,
+  3,
+  "V1"
+);
